45ac57e161db3575f2f57faba798e0e14ac4cf80
amisium <amitsaxena098@gmail.com>
2021-05-28 11:09:36 -0700

    duration and viewcount

--- flask_app.py
+++ flask_app.py
@@ -39,7 +39,7 @@ def reply_whatsapp():
             videos.append("% s (% s) (% s) (% s)" % (result["snippet"]["title"], result["id"]["videoId"], result['snippet']['description'], result['snippet']['thumbnails']['high']['url']))
             response = MessagingResponse()
             vlnk = "https://www.youtubepp.com/watch?v=" + result["id"]["videoId"]
-            duration = isodate.parse_duration(str(result['contentDetails']['duration']))
+            duration = isodate.parse_duration(str(result["contentDetails"]["duration"]))
             msg = response.message("*Title:* " + result['snippet']['title'] + "\n*Duration:* " + duration + "\n*Views:* " + result['statistics']['viewCount'] + "\n*Channel:* " + result['snippet']['channelTitle'] + "\n\n*VideoLink:* " + "```" + "https://www.youtube.com/watch?v=" + result["id"]["videoId"] + "```" + "\n\n*Download Here:* ```" + vlnk + "```\n\n*Developed by:* ```@devilHail YO```")
             msg.media(result['snippet']['thumbnails']['high']['url'])
             return str(response)
