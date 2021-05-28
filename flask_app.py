c46c0f7f9dfbb2db112f916c2d376642a183589e
amisium <amitsaxena098@gmail.com>
2021-05-28 11:05:29 -0700

    duration and viewcount

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from apiclient.discovery import build
+import isodate
 
 # Arguments that need to passed to the build function 
 DEVELOPER_KEY = "AIzaSyDjXF69w5hwq1tLbJYhe-NaMhstpp14xbE"
@@ -12,9 +13,6 @@ max_results = 3
 youtube_object = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey = DEVELOPER_KEY)
 
 app = Flask(__name__)
-
-
-#GOOD_BOY_URL = "htatps://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
 
 
 @app.route("/whatsapp", methods=["GET", "POST"])
@@ -41,7 +39,8 @@ def reply_whatsapp():
             videos.append("% s (% s) (% s) (% s)" % (result["snippet"]["title"], result["id"]["videoId"], result['snippet']['description'], result['snippet']['thumbnails']['high']['url']))
             response = MessagingResponse()
             vlnk = "https://www.youtubepp.com/watch?v=" + result["id"]["videoId"]
-            msg = response.message("*Title:* " + result['snippet']['title'] + "\n*Channel:* " + result['snippet']['channelTitle'] + "\n\n*VideoLink:* " + "```" + "https://www.youtube.com/watch?v=" + result["id"]["videoId"] + "```" + "\n\n*Download Here:* ```" + vlnk + "```\n\n*Developed by:* ```@devilHail YO```")
+            duration = isodate.parse_duration(str(result['contentDetails']['duration']))
+            msg = response.message("*Title:* " + result['snippet']['title'] + "\n*Duration:* " + duration + "\n*Views:* " + result['statistics']['viewCount'] + "\n*Channel:* " + result['snippet']['channelTitle'] + "\n\n*VideoLink:* " + "```" + "https://www.youtube.com/watch?v=" + result["id"]["videoId"] + "```" + "\n\n*Download Here:* ```" + vlnk + "```\n\n*Developed by:* ```@devilHail YO```")
             msg.media(result['snippet']['thumbnails']['high']['url'])
             return str(response)
 
