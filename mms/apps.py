9a1856ced7b6214235a0e90e53e0f46ab2dc9502
amisium <amitsaxena098@gmail.com>
2021-06-25 08:37:26 -0700

    updated .gitignore

--- mms/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MmsConfig(AppConfig):
-    name = 'mms'
--- mms/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import mms_reply
-
-urlpatterns = [
-    path('', mms_reply, name='mms_reply')
-]
